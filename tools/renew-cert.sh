6f6c2b7229e54884ee1ba06474cd83b6c451e91a
Paulo <paulosanz@poli.ufrj.br>
2020-11-13 09:23:53 -0300

    Remove forgotten commands from renew-cert.sh

--- tools/renew-cert.sh
+++ tools/renew-cert.sh
@@ -12,14 +12,7 @@ if [ "$?" -eq "0" ]; then
   screen -S monitor-iop -X quit
   screen -S startiopserver -X quit
 
-  CRON=$(ssh root@$DOMAIN "crontab -l | grep /opt/iop/run-server.sh")
+  CRON=$(crontab -l | grep /opt/iop/run-server.sh)
   COMMAND=${CRON#@reboot }
-  ssh root@$DOMAIN "screen -dmS startiopserver $COMMAND"
-
-  # Some other stuff running in the same server, although unrelated
-  screen -S monitor -X quit
-  screen -S startserver -X quit
-  CRON=$(ssh root@$DOMAIN "crontab -l | grep /root/run-server.sh")
-  COMMAND=${CRON#@reboot }
-  ssh root@$DOMAIN "screen -dmS startserver $COMMAND"
+  screen -dmS startiopserver $COMMAND
 fi
