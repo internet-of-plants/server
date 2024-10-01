e7a9850d6eee6544556cb50afdafe8e854ec9a4a
Paulo Cabral Sanz <paulo@railway.app>
2024-10-01 00:34:08 -0300

    Fix CI deploy

--- tools/run-server-with-logging.sh
+++ tools/run-server-with-logging.sh
@@ -3,4 +3,5 @@
 python3 -c "$(curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/5f852c87e5647a3cfa9ef470322aa2c788179e2c/get-platformio.py)"
 export PATH=$PATH:/home/iop/.platformio/penv/bin
 whoami
-./server 2>&1 | tee -a ./monitor.log
+cp server server-running
+./server-running 2>&1 | tee -a ./monitor.log
