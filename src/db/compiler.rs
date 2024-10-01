2bd7490ce25aa5a20fd59d02496f2fe8655199c5
Paulo Cabral Sanz <paulo@railway.app>
2024-10-01 01:29:33 -0300

    Fix typo

--- src/db/compiler.rs
+++ src/db/compiler.rs
@@ -549,11 +549,11 @@ auto prepareJson(iop::EventLoop & loop) noexcept -> iop::Api::Json {{
   return json;
 }}
 
-auto monitor(iop::EventLoop &loop, iop::AuthToken &token) noexcept -> void {{
+auto monitor(iop::EventLoop &loop, const iop::AuthToken &token) noexcept -> void {{
   loop.registerEvent(token, prepareJson(loop));
 }}
 
-auto authenticatedAct(iop::EventLoop &loop, iop::AuthToken &token) noexcept -> void {{{authenticated_actions}
+auto authenticatedAct(iop::EventLoop &loop, const iop::AuthToken &token) noexcept -> void {{{authenticated_actions}
   (void) loop;
   (void) token;
 }}
